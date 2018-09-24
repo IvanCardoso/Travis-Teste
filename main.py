ecde3fe5227c9e956f357c903e2f0e2949323079
Ivan Sandro <ivanscf18@gmail.com>
2018-09-24 19:41:55 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-'''
 
-                            Online Python Compiler.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
 
-'''
+def inversao():
+	lista=[]
 
-lista=[]
-listaf=[]
-y=0
-lista = input("Digite uma palavra pra ser invertida:  ")
-for x in range(len(lista)-1,-1,-1):
-    listaf.append(lista[x])
-    y=y+1
+	listaf=[]
 
-print(listaf)
+	y=0
+
+	lista = input("Digite uma palavra pra ser invertida:  ")
+
+	for x in range(len(lista)-1,-1,-1):
+    
+		listaf.append(lista[x])
+    
+		y=y+1
+
+
+	print(listaf)
